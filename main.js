c7c03d3cec06857c9fa88ac740cd2a390272c013
Facundo Rivarola <facu.rivarola00@gmail.com>
2023-03-13 20:46:58 -0300

    primer preentrega

---
+++ main.js
@@ -0,0 +1,23 @@
+let Remera = 500
+let Pantalon = 1200
+let total = 500 + 1200
+let productoComprado = prompt('Ingrese producto "Remera"');
+while (productoComprado != 'Remera')
+    if (productoComprado != 'Remera') {
+        alert('Vuelva a intentarlo')
+        productoComprado = prompt('Ingrese producto "Remera"')
+    }
+let productoComprado2 = prompt('Ingrese producto "Pantalon"');
+while (productoComprado2 != 'Pantalon')
+    if (productoComprado2 != 'Pantalon') {
+        alert('Vuelva a intentarlo')
+        productoComprado2 = prompt('Ingrese producto "Pantalon"')
+    } 
+    alert ('El valor total de su compra es de: ' + total)
+
+
+
+
+
+
+

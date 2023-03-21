07d77edc59c0a01a0ed9b7013c6f9ca62fa8483a
Facundo Rivarola <facu.rivarola00@gmail.com>
2023-03-21 20:02:49 -0300

    correccion

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
+
 let Remera = 500
 let Pantalon = 1200
 let total = 500 + 1200
-let productoComprado = prompt('Ingrese producto "Remera"');
-while (productoComprado != 'Remera')
-    if (productoComprado != 'Remera') {
-        alert('Vuelva a intentarlo')
-        productoComprado = prompt('Ingrese producto "Remera"')
-    }
-let productoComprado2 = prompt('Ingrese producto "Pantalon"');
-while (productoComprado2 != 'Pantalon')
-    if (productoComprado2 != 'Pantalon') {
-        alert('Vuelva a intentarlo')
-        productoComprado2 = prompt('Ingrese producto "Pantalon"')
-    } 
-    alert ('El valor total de su compra es de: ' + total)
+
+function solicitarProducto() {
+
+    let producto = prompt('Ingrese producto "Remera"');
+    while (producto != 'Remera')
+        if (producto != 'Remera') {
+            alert('Vuelva a intentarlo')
+            producto = prompt('Ingrese producto "Remera"')
+        }
+}
+function solicitarProducto2() {
+
+    let producto2 = prompt('Ingrese producto "Pantalon"');
+    while (producto2 != 'Pantalon')
+        if (producto2 != 'Pantalon') {
+            alert('Vuelva a intentarlo')
+            producto2 = prompt('Ingrese producto "Pantalon"')
+        }
+}
+function precioTotal() {
+    let total = Remera + Pantalon
+    alert ('El precio total es de '+ total)
+}
+
+solicitarProducto ()
+solicitarProducto2 ()
+precioTotal ()
 
 
 
